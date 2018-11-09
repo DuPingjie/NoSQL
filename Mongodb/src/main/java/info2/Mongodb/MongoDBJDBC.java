2e18dc2adcb93b3edd3cce987c71defc50ff01b3
DuPingjie <32465088+DuPingjie@users.noreply.github.com>
2018-11-10 00:11:37 +0100

    Add files via upload

--- Mongodb/src/main/java/info2/Mongodb/MongoDBJDBC.java
+++ Mongodb/src/main/java/info2/Mongodb/MongoDBJDBC.java
@@ -1,6 +1,10 @@
 package info2.Mongodb;
+import org.bson.Document;
+
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Indexes;
 
 public class MongoDBJDBC{
    public static void main( String args[] ){
@@ -12,14 +16,20 @@ public class MongoDBJDBC{
          MongoDatabase mongoDatabase = mongoClient.getDatabase("mydb");  
          System.out.println("Connect to database successfully");
          // create collections
-         mongoDatabase.createCollection("players");
+     /*    mongoDatabase.createCollection("players");
          System.out.println("集合players创建成功");
          mongoDatabase.createCollection("teams");
          System.out.println("集合teams创建成功");
          mongoDatabase.createCollection("matches");
          System.out.println("集合matches创建成功");
+        */ 
+         MongoCollection<Document> players = mongoDatabase.getCollection("players");
+         MongoCollection<Document> teams = mongoDatabase.getCollection("teams");
+         MongoCollection<Document> matches = mongoDatabase.getCollection("matches");
          
-         
+         players.createIndex(Indexes.ascending("name"));
+         teams.createIndex(Indexes.ascending("name"));
+         players.createIndex(Indexes.ascending("name"));
       }catch(Exception e){
         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
      }
